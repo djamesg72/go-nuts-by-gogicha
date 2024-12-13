56056838c98f6074d4b290870da465c5b0ea3db9
djamesg72 <102153892+djamesg72@users.noreply.github.com>
2024-12-13 16:52:12 +0400

    fixed

--- dist/types/components/button/button.d.ts
+++
@@ -1,7 +0,0 @@
-import { EventEmitter } from '../../stencil-public-runtime';
-export declare class MyComponent {
-    direction: 'left-top' | 'right-top' | 'left-bottom' | 'right-bottom';
-    gChooseDirection: EventEmitter;
-    chooseDirection(): void;
-    render(): any;
-}
